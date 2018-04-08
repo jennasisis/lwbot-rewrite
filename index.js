80cc820205b780ff3cb18e8534de67901b54201a
jennasisis <jennasisis@gmail.com>
2018-04-07 20:26:12 -0400

    dw about this

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-/* eslint-disable */
-
 if (process.version.slice(1).split(`.`)[0] < 8) throw new Error(`Node 8.0.0 or higher is required. Update Node on your system.`);
 
 const Discord = require(`discord.js`);
-const Sequelize = require('sequelize');
+const Sequelize = require(`sequelize`);
 const { promisify } = require(`util`);
 const readdir = promisify(require(`fs`).readdir);
 const Enmap = require(`enmap`);
 const EnmapLevel = require(`enmap-level`);
 const client = new Discord.Client();
 
-const sequelize = new Sequelize('database', 'user', 'password', {
-    host: 'localhost',
-    dialect: 'sqlite',
-    logging: false,
-    // SQLite only
-    storage: 'database.sqlite',
+const sequelize = new Sequelize(`database`, `user`, `password`, {
+  host: `localhost`,
+  dialect: `sqlite`,
+  logging: false,
+  // SQLite only
+  storage: `database.sqlite`,
 });
-const Tags = sequelize.define('tags', {
-    name: {
-        type: Sequelize.STRING,
-        unique: true,
-    },
-    description: Sequelize.TEXT,
-    username: Sequelize.STRING,
-    usage_count: {
-        type: Sequelize.INTEGER,
-        defaultValue: 0,
-        allowNull: false,
-    },
+const Tags = sequelize.define(`tags`, {
+  name: {
+    type: Sequelize.STRING,
+    unique: true,
+  },
+  description: Sequelize.TEXT,
+  username: Sequelize.STRING,
+  usage_count: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0,
+    allowNull: false,
+  },
 });
 
 client.config = require(`./config.js`);
@@ -41,36 +39,36 @@ client.settings = new Enmap({provider: new EnmapLevel({name: `settings`})});
 
 const init = async () => {
 
-    // Here we load **commands** into memory, as a collection, so they're accessible
-    // here and everywhere else.
-    const cmdFiles = await readdir(`./commands/`);
-    client.logger.log(`Loading a total of ${cmdFiles.length} commands.`);
-    cmdFiles.forEach(f => {
-        if (!f.endsWith(`.js`)) return;
-        const response = client.loadCommand(f);
-        if (response) console.log(response);
-    });
+  // Here we load commands into memory, as a collection, so they're accessible
+  // here and everywhere else.
+  const cmdFiles = await readdir(`./commands/`);
+  client.logger.log(`Loading a total of ${cmdFiles.length} commands.`);
+  cmdFiles.forEach(f => {
+    if (!f.endsWith(`.js`)) return;
+    const response = client.loadCommand(f);
+    if (response) console.log(response);
+  });
 
-    // Then we load events, which will include our message and ready event.
-    const evtFiles = await readdir(`./events/`);
-    client.logger.log(`Loading a total of ${evtFiles.length} events.`);
-    evtFiles.forEach(file => {
-        const eventName = file.split(`.`)[0];
-        const event = require(`./events/${file}`);
-        // This line is awesome by the way. Just sayin'.
-        client.on(eventName, event.bind(null, client));
-        delete require.cache[require.resolve(`./events/${file}`)];
-    });
+  // Then we load events, which will include our message and ready event.
+  const evtFiles = await readdir(`./events/`);
+  client.logger.log(`Loading a total of ${evtFiles.length} events.`);
+  evtFiles.forEach(file => {
+    const eventName = file.split(`.`)[0];
+    const event = require(`./events/${file}`);
+    // This line is awesome by the way. Just sayin'.
+    client.on(eventName, event.bind(null, client));
+    delete require.cache[require.resolve(`./events/${file}`)];
+  });
 
-    // Generate a cache of client permissions for pretty perms
-    client.levelCache = {};
-    for (let i = 0; i < client.config.permLevels.length; i++) {
-        const thisLevel = client.config.permLevels[i];
-        client.levelCache[thisLevel.name] = thisLevel.level;
-    }
+  // Generate a cache of client permissions for pretty perms
+  client.levelCache = {};
+  for (let i = 0; i < client.config.permLevels.length; i++) {
+    const thisLevel = client.config.permLevels[i];
+    client.levelCache[thisLevel.name] = thisLevel.level;
+  }
 
-    // Here we login the client.
-    client.login(client.config.token);
+  // Here we login the client.
+  client.login(client.config.token);
 
 // End top-level async/await function.
 };
